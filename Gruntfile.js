0b18970d8d0345f071db82f3d1b7a6dff5f8d4d8
Vijay <vjscjp09@gmail.com>
2015-09-30 23:44:00 +0530

    Shipped Docker related file

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
         connect : {
             root : {
                 options : {
-                    port : 8001,
+                    port : 3000,
                     keepalive : true,
                     host: '*'
                 }
